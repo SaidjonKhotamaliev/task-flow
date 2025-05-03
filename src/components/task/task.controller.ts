578a68c2b58f40bc1872567cf385378d2849fcc3
SaidjonKhotamaliev <saidjon.khotamaliev@gmail.com>
2025-05-03 15:08:33 +0500

    feat: develop getMyTasks REST api

--- src/components/task/task.controller.ts
+++ src/components/task/task.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { Task, taskInput } from 'src/libs/dtos/task';
 import { TaskService } from './task.service';
 import { Request } from 'express';
+import { ObjectId } from 'mongoose';
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
@@ -14,14 +24,18 @@ export class TaskController {
     return await this.taskService.createTask(input);
   }
 
-  // @Get('my-tasks')
-  // @UseGuards(AuthGuard)
-  // public async getMyTasks(@Req() req: Request): Promise<Task[]> {
-  //   console.log('GET, getMyTasks');
-  //   console.log(req.body);
-  //   const userId = req.body.authMember._id;
-  //   return await this.boardService.getMyTasks(userId);
-  // }
+  @Get('getMyTasks')
+  @UseGuards(AuthGuard)
+  public async getMyTasks(
+    @Req() req: Request,
+    @Query('boardId') boardId: string,
+  ): Promise<Task[]> {
+    if (!boardId) {
+      throw new BadRequestException('boardId is required');
+    }
+
+    return await this.taskService.getMyTasks(boardId);
+  }
 
   //   @Post('updateTask')
   //   @UseGuards(AuthGuard)
--- src/components/task/task.service.ts
+++ src/components/task/task.service.ts
@@ -1,7 +1,8 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { Task, taskInput } from 'src/libs/dtos/task';
+import { shapeIntoMongoObjectId } from 'src/libs/types/config';
 
 @Injectable()
 export class TaskService {
@@ -13,6 +14,22 @@ export class TaskService {
       return result;
     } catch (err) {
       console.log('Error on  createBoard', err);
+      throw new BadRequestException(err.message);
+    }
+  }
+
+  public async getMyTasks(boardId: string): Promise<Task[]> {
+    try {
+      boardId = shapeIntoMongoObjectId(boardId);
+      const result: Task[] = await this.boardModel
+        .find({ boardId: boardId })
+        .exec();
+
+      console.log('result: ', result);
+
+      return result;
+    } catch (err) {
+      console.log('Error on  getMyTasks', err);
       throw new BadRequestException(err.message);
     }
   }
